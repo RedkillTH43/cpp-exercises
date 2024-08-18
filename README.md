e985d46ad9df343b2b5a4299bd9b07b67d69d082
RedkillTH43 <johnlawrencetamio@gmail.com>
2024-08-18 10:46:53 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# G12 TVL C++ Exercises
+A repository for coding exercises for C++ G12 TVL. 
---
+++ average.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int firstNum;
+  int secondNum;
+  int thirdNum;
+  float result;
+  
+  cout << " -- Average of 3 Numbers -- " << endl;
+  cout << "Input 3 numbers" << endl;
+  cout << "First Number: ";
+  cin >> firstNum;
+  cout << "Second Number: ";
+  cin >> secondNum;
+  cout << "Third Number: ";
+  cin >> thirdNum;
+  
+  result = (firstNum + secondNum + thirdNum) / 3.0f;
+  
+  cout << "The average is " << result << endl;
+  return 0;
+}
