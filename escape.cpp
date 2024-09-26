2b3bcfadf780ac324143a9c0faf1b91374ea867c
RedkillTH43 <johnlawrencetamio@gmail.com>
2024-09-26 16:36:29 +0800

    Added escape.cpp

---
+++ escape.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "\"Good Afternoon\"\n\t\\Welcome!\\\n\n\'This is programming course\'\n\n          J\n          J\n          J\n          J\n          J\n          J\nJ         J\nJ         J\n J       J\n  J     J\n   J J J   \n";
+  return 0;
+}
